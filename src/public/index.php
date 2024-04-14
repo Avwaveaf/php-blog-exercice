53275246d069c9a23a3c99d9e330c39c92297983
Afif <49422146+Avwaveaf@users.noreply.github.com>
2024-04-14 18:55:57 +0700

    refactoring router path

--- src/public/index.php
+++ src/public/index.php
@@ -7,28 +7,15 @@ $root = __DIR__ . DIRECTORY_SEPARATOR;
 
 define("VIEWS_PATH", $root .'../views' .DIRECTORY_SEPARATOR);
 define("CONTROLLERS_PATH", $root . '../app/controllers' . DIRECTORY_SEPARATOR);
+define("ROUTES_PATH", $root . '../app/routes' . DIRECTORY_SEPARATOR);
 define("UTILS_PATH", $root . '../app/utils' . DIRECTORY_SEPARATOR);
 
 // require all utils
 require_once UTILS_PATH . 'dev_dump.php';
 
 
-// views path
+//require the router 
+require_once ROUTES_PATH . 'router.php';
 
-// parse the request url to an array of path and query
-$request_uri = parse_url($_SERVER['REQUEST_URI']);
-$path = $request_uri['path'];
 
-$routes = [
-    '/' => CONTROLLERS_PATH . 'home.php',
-    '/contact' => CONTROLLERS_PATH . 'contact.php',
-    '/about' => CONTROLLERS_PATH . 'about.php',
-];
 
-// Check if the path exists in the routes array
-if (array_key_exists($path, $routes)) {
-    require_once $routes[$path];
-} else {
-    // Route not found, include 404 page
-    require_once VIEWS_PATH . '404page.php';
-}
